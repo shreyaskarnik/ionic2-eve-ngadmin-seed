755a07aee071782062320d0d7d3a857c3ba6e91d
Shreyas Karnik <karnik.shreyas@gmail.com>
2016-11-03 16:56:47 -0500

    .gitignore is now working

--- mobile-client/SeedProject/src/app/app.component.ts
+++ mobile-client/SeedProject/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
-import { HelloIonicPage } from './pages/hello-ionic/hello-ionic';
-import { ListPage } from './pages/list/list';
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ListPage } from '../pages/list/list';
 
 
 @Component({
--- mobile-client/SeedProject/src/app/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  API_HOST: "http://api:8888",
-  DEBUG: true,
-  NAME: "dev"
-};
